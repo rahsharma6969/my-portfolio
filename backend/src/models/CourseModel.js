cb30066e58fb4f7a47d12a7a70dc1cae05f2272c
Rahul Sharma <139571843+rahsharma6969@users.noreply.github.com>
2026-07-02 11:33:16 +0530

    Delete backend directory

--- backend/src/models/CourseModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const courseSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    isFree: { type: Boolean, default: true },
-});
-
-export default mongoose.model("Course", courseSchema);
