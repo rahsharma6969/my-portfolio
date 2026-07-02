3d73cf01c535c8aee29127e8447db89f286754d5
Rahul Sharma <139571843+rahsharma6969@users.noreply.github.com>
2026-07-02 11:33:31 +0530

    Delete frontend directory

--- frontend/src/config/axiosConifg.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-console.log(import.meta.env.VITE_BACKEND_API_URL)
-
-export default axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_URL,
-  withCredentials: true
-});
--- frontend/src/hooks/useAdmin.js
+++
@@ -1,6 +0,0 @@
-import { AdminContext } from "../context/AdminContext";
-import { useContext } from "react";
-
-export const useAdmin = () => {
-  return useContext(AdminContext);
-};
